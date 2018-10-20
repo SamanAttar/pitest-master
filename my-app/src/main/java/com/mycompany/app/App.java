0659020c9934c3196ea69c35dbc9130839cb60f3
Saman Attar <saman.attar@outlook.com>
2018-10-19 19:04:33 -0500

    Add App to test mutants

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,33 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+  public void App(){
+    new App();
+  }
+
+  public static int add(int a, int z)
+  {
+    return z+a;
+  }
+
+  public static boolean compare(int a, int z)
+  {
+    if(a == 0)
+      return true;
+    return false;
+  }
+}
+
+
+/*
+
+public static void main( String[] args )
+{
+    System.out.println( "Hello World!" );
+}
+*/
